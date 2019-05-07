dcda5bbe393f24276cdb66b6e540451127d42c75
BrayanJakes <brianjakes22@gmail.com>
2019-05-07 17:56:47 -0400

    mas arreglos

--- server.js
+++ server.js
@@ -6,10 +6,18 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/appdemo'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+     res.header("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE, OPTIONS");
+    next();
+});
 
 app.set('port', process.env.PORT || 8080)
 
-app.get('/*', function(req,res) {
+app.get('*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/appdemo/index.html'));
 });
