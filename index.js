32ac2d0d9eb9b9afaaeabc2f51ece6709b4a256b
BrayanJakes <brianjakes22@gmail.com>
2019-05-07 23:12:58 -0400

    eee

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.static(__dirname + '/dist/appdemo'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://solicitud-webapps.herokuapp.com/usuario');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
